4e2a3802759acdf2155700b23ef4751591daca6f
qaisarm <qmphy99@gmail.com>
2022-11-04 08:29:07 +0100

    changed image

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,7 @@
+import Image from "next/image";
 import { useRouter } from 'next/router';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
-
 function MeetupItem(props) {
   const router = useRouter();
 
