96325b31f28c82519c7d4feba072d2929e88c04e
qaisarm <qmphy99@gmail.com>
2022-11-04 08:21:57 +0100

    changed image

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,10 +1,10 @@
-import { Fragment } from "react"
+import Image from "next/image";
 import classes from './MeetupDetail.module.css'
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-        <img
+        <Image
         src={props.image}
         alt={props.title}/>
         <h1> {props.title}</h1>
